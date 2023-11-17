aea00929770e96cf9cf5720a5e0611a82f934f58
yyyr-otz <32903075+yyyr-otz@users.noreply.github.com>
2023-11-18 01:12:46 +0800

    Update XrayPing.py

--- check/xray_ping/XrayPing.py
+++ check/xray_ping/XrayPing.py
@@ -103,7 +103,7 @@ class XrayPing:
         configFilePath = "./xray_config_ping.json"
         with open(configFilePath, 'w') as f:
             f.write(confFinalStr)
-
+        subprocess.run(['chmod', '755', 'check/xray_ping/xray'])
         runXrayThread = Thread(target=subprocess.run,
                                args=([Path("check/xray_ping/xray").resolve(), "run", "-c", configFilePath],))
         runXrayThread.daemon = True
