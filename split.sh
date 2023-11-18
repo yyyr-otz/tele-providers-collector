a91a89aec3455bb0c7ef246b20083f6e92f988a5
yyyr-otz <32903075+yyyr-otz@users.noreply.github.com>
2023-11-19 05:12:55 +0800

    Update split.sh

--- split.sh
+++ split.sh
@@ -1,4 +1,4 @@
-file_rows=wc -l ./collected-proxies/row-url/all.txt
+file_rows=wc -l ./collected-proxies/row-url/all.txt|awk '{print 1}'
 file_num=3
 file_num_row=$((${file_rows} + 2))
 every_file_row=$((${file_num_row}/${file_num}))
