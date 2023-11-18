83fc69fdab9b38230b11af0acd4c1d1b518ce57d
yyyr-otz <32903075+yyyr-otz@users.noreply.github.com>
2023-11-19 04:53:15 +0800

    Add files via upload

---
+++ .github/split.sh
@@ -0,0 +1,5 @@
+file_rows=wc -l ./collected-proxies/row-url/all.txt
+file_num=3
+file_num_row=$((${file_rows} + 2))
+every_file_row=$((${file_num_row}/${file_num}))
+split -d -a 1 -l ${every_file_row} ./collected-proxies/row-url/all.txt ./collected-proxies/row-url/all_
