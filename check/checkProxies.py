6946659c532197e91bc03e1240da8a0839ba194d
yyyr-otz <32903075+yyyr-otz@users.noreply.github.com>
2023-11-20 03:52:53 +0800

    Update checkProxies.py

--- check/checkProxies.py
+++ check/checkProxies.py
@@ -3,6 +3,7 @@ import sys
 import uuid
 from ruamel.yaml import YAML
 from gitRepo import commitPushRActiveProxiesFile, getLatestActiveConfigs
+import shutil
 
 sys.path.append('./check/xray_url_decoder/')
 sys.path.append('./check/clash_meta_url_decoder/')
@@ -35,7 +36,7 @@ def is_buggy_in_clash_meta(config: ClashMetaDecoder):
 """
 # 根据序号选择文件
 #with open("collected-proxies/row-url/all" + args.n + ".txt", 'r') as rowProxiesFile:
-with open("collected-proxies/row-url/all.txt", 'r') as rowProxiesFile:
+with open("./collected-proxies/row-url/all.txt", 'r') as rowProxiesFile:
     configs = []
 #    clash_meta_configs = []
 #    for_game_proxies = []
@@ -72,8 +73,8 @@ with open("collected-proxies/row-url/all.txt", 'r') as rowProxiesFile:
     # 序号传给get
     # getLatestActiveConfigs(args.n)
 
-#    with open("collected-proxies/xray-json/active_all" + args.n + ".txt", 'w') as activeProxiesFile:
-    with open("collected-proxies/xray-json/active_all.txt", 'w') as activeProxiesFile:
+#    with open("collected-proxies/xray-json/actives_all" + args.n + ".txt", 'w') as activeProxiesFile:
+    with open("./collected-proxies/xray-json/actives_all.txt", 'w') as activeProxiesFile:
         for active in delays.actives:
             activeProxiesFile.write(json.dumps(active['proxy']) + "\n")
     
@@ -111,3 +112,72 @@ with open("collected-proxies/row-url/all.txt", 'r') as rowProxiesFile:
 #time.sleep(time_n * 200)
 
 # commitPushRActiveProxiesFile(args.n)
+def is_duplicated_config(proxy: str, seen_lines: set[str]):
+    isDuplicated = False
+
+    configs: list[XrayUrlDecoder] = []
+    for url in seen_lines:
+        if len(url) > 10:
+            try:
+                configs.append(XrayUrlDecoder(url))
+            except:
+                pass
+
+    try:
+        c_str = XrayUrlDecoder(proxy).generate_json_str()
+        for conf in configs:
+            if conf.is_equal_to_config(c_str):
+                isDuplicated = True
+    except:
+        pass
+
+    return isDuplicated
+
+
+def keep_only_lines_and_remove_duplicates(file_path, lines_to_keep):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    if lines_to_keep is None:
+        new_lines = lines
+    else:
+        lines_to_keep = set(lines_to_keep)  # Convert to a set for faster lookup
+        new_lines = [line for i, line in enumerate(lines, start=1) if i in lines_to_keep]
+
+    unique_lines = []
+    seen_lines = set()
+    for line in new_lines:
+        if line not in seen_lines:
+            if not is_duplicated_config(line, seen_lines):
+                unique_lines.append(line)
+            seen_lines.add(line)
+
+    new_content = '\n'.join(line.rstrip() for line in unique_lines if line.strip())
+
+    with open(file_path, 'w') as file:
+        file.write(new_content)
+
+
+# getLatestActiveConfigs()
+# getLatestRowProxies()
+
+lineNumberOfFounds = []
+with open("collected-proxies/xray-json/actives_all.txt", 'r') as activeProxiesFile:
+    for activeConfig in activeProxiesFile:
+        if len(activeConfig) < 10: continue
+
+        with open("collected-proxies/row-url/all.txt", 'r') as rowProxiesFile:
+            # remove if it's not in active proxies
+            for (index, rowProxyUrl) in enumerate(rowProxiesFile):
+                if len(rowProxyUrl) < 10: continue
+
+                try:
+                    config = XrayUrlDecoder(rowProxyUrl)
+                    if config.isSupported and config.isValid and config.is_equal_to_config(activeConfig):
+                        lineNumberOfFounds.append(index + 1)
+                except:
+                    pass
+
+shutil.copyfile("collected-proxies/row-url/all.txt", "collected-proxies/row-url/actives_now")
+
+keep_only_lines_and_remove_duplicates("collected-proxies/row-url/actives_now", lineNumberOfFounds)
