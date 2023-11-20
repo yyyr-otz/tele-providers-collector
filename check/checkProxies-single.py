04a5e3079b9b21f7a55331c2286c401593db01c7
yyyr-otz <32903075+yyyr-otz@users.noreply.github.com>
2023-11-21 01:47:53 +0800

    Update checkProxies-single.py

--- check/checkProxies-single.py
+++ check/checkProxies-single.py
@@ -200,4 +200,5 @@ with open("collected-proxies/xray-json/actives_all.txt", 'r') as activeProxiesFi
 
 shutil.copyfile("collected-proxies/row-url/all.txt", "collected-proxies/row-url/actives_now.txt")
 
+keep_only_lines_and_remove_duplicates("collected-proxies/row-url/result_actives_now.txt", lineNumberOfFounds_ALL)
 keep_only_lines_and_remove_duplicates("collected-proxies/row-url/actives_now.txt", lineNumberOfFounds)
